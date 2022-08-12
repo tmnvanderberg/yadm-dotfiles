6174382523959e0ccbf66c7ba0fb5e0b5ad68a8a
Timon van der Berg <timon.vanderberg@streamunlimited.com>
2022-08-12 16:55:49 +0200

    watson

--- .config/nvim/lua/search.lua
+++ .config/nvim/lua/search.lua
@@ -28,15 +28,34 @@ Map(
 	require'fzf-lua'.fzf_exec("watson projects", {
 		 actions = {
 			['default'] = function(selected, opts)
-				vim.cmd(":!watson stop")
 				vim.cmd(":!watson start " .. selected[1])
 			end
 		 }
 	})
 end
 
+function WatsonReport()
+	local buf = vim.api.nvim_create_buf(false, true)
+	-- vim.api.nvim_buf_set_lines(buf, 0, -1, true, {"test", "text"})
+	local handle = io.popen("watson report --all --no-pager")
+	if handle ~= nil then
+		local result = handle:read("*a")
+		vim.api.nvim_buf_set_text(buf, 0, 0, 0, 0, result)
+		handle:close()
+	end
+	local opts = {
+	    	relative = 'editor',
+		width = 50,
+		height = 50,
+		col = 100,
+		row = 50,
+		anchor = 'NW',
+		border = "single",
+	}
+	local win = vim.api.nvim_open_win(buf, 0, opts)
+end
 
- function WatsonCommands()
+function WatsonCommands()
 	require'fzf-lua'.fzf_exec(
 		{"status", "start", "stop", "log", "aggregate", "report", "cancel"},
 		{
@@ -48,7 +67,7 @@ end
 					elseif  cmd == "status" 	then 	vim.cmd(":!watson status")
 					elseif 	cmd == "log" 		then 	vim.cmd(":new | 0read ! watson log --all --no-pager")
 					elseif 	cmd == "aggregate" 	then 	vim.cmd(":new | 0read ! watson aggregate --no-pager")
-					elseif 	cmd == "report" 	then 	vim.cmd(":new | 0read ! watson report --all --no-pager")
+					elseif 	cmd == "report" 	then 	WatsonReport()	
 					elseif 	cmd == "cancel" 	then 	vim.cmd(":new | 0read ! watson cancel")
 					end
 				end
