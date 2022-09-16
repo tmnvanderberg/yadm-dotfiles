022923b3d32e971438a6c01a7f3f97af16678787
Timon van der Berg <timon.vanderberg@streamunlimited.com>
2022-09-16 15:24:42 +0200

    .

--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -1,7 +1,7 @@
 -- set the color scheme
-vim.cmd [[ set background=dark ]]
+vim.cmd [[ set background=light ]]
 vim.cmd [[ set termguicolors ]]
-local ok, _ = pcall(vim.cmd, 'colorscheme gruvbox-material')
+local ok, _ = pcall(vim.cmd, 'colorscheme peachpuff')
 if not ok then
 	vim.cmd [[ colorscheme default ]]
 end
