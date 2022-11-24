d742c857ae35f46a1da722a73e05f6699547f9a5
Timon van der Berg <tmnvanderberg@gmail.com>
2022-11-24 22:39:09 +0100

    fix things

--- .config/nvim/lua/language.lua
+++ .config/nvim/lua/language.lua
@@ -41,7 +41,13 @@ local set_buffer_maps = function(client, bufnr)
   vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', vim.lsp.buf.format{{ async = true }}, bufopts)
+  vim.keymap.set(
+  'n',
+  '<space>f',
+  function()
+    vim.lsp.buf.format({ async = true })
+  end,
+  bufopts)
 end
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
