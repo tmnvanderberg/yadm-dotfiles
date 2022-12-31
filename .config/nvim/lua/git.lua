7fb4a93e9566bc442bbab2e6d91902c4cccb5583
Timon van der Berg <tmnvanderberg@gmail.com>
2022-12-31 19:31:03 +0100

    nvim: remove merginal leftovers

--- .config/nvim/lua/git.lua
+++ .config/nvim/lua/git.lua
@@ -1,7 +1,5 @@
 require('map')
 
-vim.g.merginal_windowWidth = 50
-
 -- main git interface of fugitive
 Map(
 	"n",
@@ -15,13 +13,5 @@ Map(
 	"n",
 	"<Leader>t",
 	":G cherry-pick <C-R><C-W><CR>",
-	{ silent = true }
-)
-
--- List and manipulate branches
-Map(
-	"n",
-	"<Leader>M",
-	":Merginal<CR>",
 	{ silent = true }
 )
