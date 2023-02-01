9f7eabd184b4db489078d24af823bdbff888a46f
Timon van der Berg <timon.vanderberg@streamunlimited.com>
2023-02-01 13:59:25 +0100

    enable line numbers

--- .config/nvim/lua/basic.lua
+++ .config/nvim/lua/basic.lua
@@ -5,7 +5,7 @@ vim.g.mapleader = ' '
 vim.cmd [[set mouse=a]]
 
 -- enable line numbers
-vim.wo.number = false
+vim.wo.number = true
 
 -- enable autocomplete
 vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
