ec7b8bba5e7d294fd7830e0950457f6059ba867c
Timon van der Berg <timon.vanderberg@streamunlimited.com>
2024-03-19 17:27:36 +0100

    change neovim theme to gruvbox

--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -1,7 +1,7 @@
 -- set the color scheme
 vim.cmd [[ set termguicolors ]]
 vim.cmd [[ set background=dark]]
-local ok, _ = pcall(vim.cmd, 'colorscheme carbonfox')
+local ok, _ = pcall(vim.cmd, 'colorscheme gruvbox-material')
 if not ok then
 	vim.cmd [[ colorscheme default ]]
 end
