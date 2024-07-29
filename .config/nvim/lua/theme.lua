cf1d17ad60dd5005f3d440ba5276377da5765b04
Timon van der Berg <timon.vanderberg@streamunlimited.com>
2024-07-29 17:05:12 +0200

    theme: gruvbox-material

--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -1,8 +1,7 @@
 -- set the color scheme
 vim.cmd [[ set termguicolors ]]
-vim.cmd [[ set background=light]]
--- local ok, _ = pcall(vim.cmd, 'colorscheme catppuccin-frappe')
-local ok, _ = pcall(vim.cmd, 'colorscheme dayfox')
+vim.cmd [[ set background=dark]]
+local ok, _ = pcall(vim.cmd, 'colorscheme gruvbox-material')
 if not ok then
 	vim.cmd [[ colorscheme default ]]
 end
