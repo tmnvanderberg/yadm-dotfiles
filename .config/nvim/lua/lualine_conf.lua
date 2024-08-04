c511d7e68905ac51afa1dff1c62869430c0e3f6e
Timon van der Berg <tmnvanderberg@gmail.com>
2024-08-04 13:19:01 +0200

    nvim: lualine: update conf
    Signed-off-by: Timon van der Berg <tmnvanderberg@gmail.com>

--- .config/nvim/lua/lualine_conf.lua
+++ .config/nvim/lua/lualine_conf.lua
@@ -1,9 +1,17 @@
+local function get_git_toplevel_basename()
+  local handle = io.popen("basename `git rev-parse --show-toplevel`")
+  local result = handle:read("*a")
+  handle:close()
+  -- return result
+  return result:match("^%s*(.-)%s*$") -- trim any whitespace
+end
+
 require('lualine').setup {
   options = {
     icons_enabled = true,
-    theme = 'auto',
-    component_separators = { left = '', right = '' },
-    section_separators = { left = '', right = '' },
+    theme = 'gruvbox',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
     disabled_filetypes = {
       statusline = {},
       winbar = {},
@@ -18,10 +26,10 @@ require('lualine').setup {
   },
   sections = {
     lualine_a = { 'mode' },
-    lualine_b = { 'branch', 'diff', 'diagnostics' },
-    lualine_c = { 'filename' },
+    lualine_b = { 'hostname', get_git_toplevel_basename, 'branch', 'diagnostics' },
+    lualine_c = { {'filename', path = 3} },
     -- lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_x = { 'filetype' },
+    lualine_x = { 'searchcount', 'filetype' },
     lualine_y = { 'progress' },
     lualine_z = { 'location' }
   },
@@ -36,5 +44,6 @@ require('lualine').setup {
   tabline = {},
   winbar = {},
   inactive_winbar = {},
-  extensions = {}
+  extensions = {'nvim-tree', 'fzf', 'lazy', 'quickfix', 'fugitive'}
 }
+
