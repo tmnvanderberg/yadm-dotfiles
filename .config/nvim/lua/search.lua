c3e4f69bce9807eb9eabd54d5ed67d387ea622b3
Timon van der Berg <timon.vanderberg@streamunlimited.com>
2024-11-29 10:11:11 +0100

    nvim: remove spectre

--- .config/nvim/lua/search.lua
+++ .config/nvim/lua/search.lua
@@ -1,6 +1,5 @@
 require('map')
 
--- Fuzzy search for anything, really.
 Map(
 	"n",
 	"<C-p>",
@@ -9,80 +8,15 @@ Map(
 )
 
 Map(
-	"n",
-	"<C-l>",
-	":FzfLua buffers<CR>",
-	{ silent = true, noremap = true }
-)
-
--- search current word with with spectre
-Map(
   "n",
   "<Leader>w",
-  ":lua require('spectre').open_visual({select_word=true})<CR>",
+  ":lua require('grug-far').open({ prefills = { search = vim.fn.expand('<cword>') }})<CR>",
   { silent = true, noremap = true }
 )
--- search current selection with spectre
+
 Map(
   "v",
   "<Leader>w",
-  ":lua require('spectre').open_visual()<CR>",
+  ":lua require('grug-far').with_visual_selection()<CR>",
   { silent = true, noremap = true }
 )
-
-require('spectre').setup({
-  open_cmd = 'botright new',
-  find_engine = {
-    -- rg is map with finder_cmd
-    ['rg'] = {
-      cmd = "rg",
-      -- default args
-      args = {
-        '--color=never',
-        '--no-heading',
-        '--with-filename',
-        '--line-number',
-        '--column',
-      },
-      options = {
-        ['ignore-case'] = {
-          value= "--ignore-case",
-          icon="[I]",
-          desc="ignore case"
-        },
-        ['hidden'] = {
-          value="--hidden",
-          desc="hidden file",
-          icon="[H]"
-        },
-        ['no-ignore-vcs'] = {
-          value="--no-ignore-vcs",
-          desc="ignore gitignore",
-          icon="[NIVCS]"
-        },
-        -- you can put any rg search option you want here it can toggle with
-        -- show_option function
-      }
-    },
-    ['ag'] = {
-      cmd = "ag",
-      args = {
-        '--vimgrep',
-        '-s'
-      } ,
-      options = {
-        ['ignore-case'] = {
-          value= "-i",
-          icon="[I]",
-          desc="ignore case"
-        },
-        ['hidden'] = {
-          value="--hidden",
-          desc="hidden file",
-          icon="[H]"
-        },
-      },
-    },
-  },
-})
-
